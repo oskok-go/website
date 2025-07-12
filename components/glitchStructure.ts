1e6b2ed1ee7ccf8da39c8825466eb76798112c19
oskok-go <oskok927@gmail.com>
2025-07-12 21:24:17 +0200

    Add files via upload

---
+++ components/glitchStructure.ts
@@ -0,0 +1,18 @@
+export const glitchStructure: Record<string, string[]> = {
+  default: [
+    "https://cold1.gofile.io/download/direct/453f5479-8ab3-4055-8981-edb06a4914a3/glitch3.mp4",
+    "https://store5.gofile.io/download/direct/f4d5d4d2-6741-4d60-9a6e-6b02fc10ed90/glitch1.mp4",
+    "https://store6.gofile.io/download/direct/e53319dd-5ac2-43bc-b992-27f42b3c28e3/glitch2.mp4",
+    "https://store2.gofile.io/download/direct/bf3adfeb-a629-4907-abad-aa4e7a1ed4c1/glitch4.mp4",
+    "https://store5.gofile.io/download/direct/189604ec-57cd-41fa-a474-153110dae4ed/glitch5.mp4",
+    "https://store5.gofile.io/download/direct/cd4dfbb2-0b54-4626-8e3c-75b61c2cc068/glitch6.mp4",
+  ],
+  retro: [
+    "https://yourcdn.com/glitch/retro1.mp4",
+    "https://yourcdn.com/glitch/retro2.mp4",
+  ],
+  matrix: [
+    "https://yourcdn.com/glitch/matrix1.mp4",
+    "https://yourcdn.com/glitch/matrix2.mp4",
+  ],
+};
